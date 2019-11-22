50b5b8110609a1b2275eafe0a912b795f120de19
gonearewe <cristmactavish@outlook.com>
2019-11-23 07:43:08 +0800

    update unit 1

--- reports/1/1-1.m
+++ reports/1/1-1.m
@@ -1,5 +1,12 @@
-t=-7:0.11:7;
+t=-20:0.01:20;
 subplot(2,3,1);
 plot(t, sinc(t));
 subplot(2,3,2);
-plot(t,square(t));
+plot(t,rectpuls(t,2));
+subplot(2,3,3);
+plot(t,square(t,2));
+subplot(2,3,4);
+plot(t,tripuls(t,0.5));
+subplot(2,3,5);
+plot(t,sawtooth(t,0.5));
+
--- reports/1/1-2.m
+++ reports/1/1-2.m
@@ -1,4 +1,33 @@
-t=0:0.11:5;
-subplot(2,2,1);
-plot(t, 2-exp(-2.*t));
+% 符号法
+syms t; % be careful performance issue.
+f=sym('(2-exp(-2*t))*heaviside(t)');
+ezplot(f,[-1,10]);
+f=sym('cos(pi*t/2)*(heaviside(t)-heaviside(t-4))');
+ezplot(f,[-2,10]);
+f=sym('exp(t)*cos(t)*heaviside(t)');
+ezplot(f,[-2,10]);
+f=sym('2/3*t*heaviside(t+2)');
+ezplot(f,[-10,10]);
+
+% 数值法
+t=-1,0.01,10;
+y1=stepfun(t,0);
+y=y1.*(2-exp(-2.*t));
+plot(t,y);
 subplot(2,2,2);
+
+t=-1,0.01,10;
+f1=stepfun(t,0)-stepfun(t,4);
+f=(cos(pi*t/2)).*f1;
+plot(t,f);
+
+t=-2,0.01,10;
+f1=stepfun(t,0);
+f=(exp(t).*cos(t)).*f1;
+plot(t,f);
+
+t=-10,0.001,10;
+f1=stepfun(t,-2);
+f=2/3.*t.*f1;
+plot(t,f);
+

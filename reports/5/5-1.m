2a1155978d5831874fc1aa31ce4037b129f3023b
gonearewe <cristmactavish@outlook.com>
2019-12-07 15:30:23 +0800

    unit 5

---
+++ reports/5/5-1.m
@@ -0,0 +1,23 @@
+syms t;
+y=sinc(t);
+ezplot(y,[-10,10,-0.3,1]);
+grid on;
+figure;
+T=-24:0.8:24;
+fs=sinc(T);
+
+grid on;
+stem(T,fs);
+title('抽样');
+figure;
+deltaT=0.8;
+w=-14*pi:0.009:14*pi;
+F=deltaT*fs*exp(-j*T'*w);
+plot(w,abs(F));
+title('幅度');
+figure;
+wc=4;
+X=-24:0.8:24;
+f1=(deltaT*wc/pi)*fs*sinc(wc/pi*(ones(length(T),1)*X-T'*ones(1,length(X))));
+plot(X,f1);
+title('还原');
